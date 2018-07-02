d2d595f1a394807d4d790b31b7ac0be1de307cfa
alsuehrckwl <alsuehrckwl@naver.com>
2018-07-03 01:17:39 +0900

    mobx init

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
+import { Provider } from 'mobx-react';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.scss';
 import registerServiceWorker from './registerServiceWorker';
+// import stores from './stores';
+import todoStore from './stores/todoStore';
 
 ReactDOM.render(
-  <App />,
+  <Provider todoStore={todoStore}>
+    <App />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
